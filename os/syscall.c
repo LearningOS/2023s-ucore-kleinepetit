4cc8507840fed58d5b4d0ab3b1720935555c661e
kleinepetit <xxaassdd@qq.com>
2023-04-23 13:37:19 +0800

    update

--- os/syscall.c
+++ os/syscall.c
@@ -60,6 +60,7 @@ void syscall()
 	*/
 	curr_proc()->info->syscall_times[id]++;
 	curr_proc()->info->syscall_times[124]++;
+	curr_proc()->info->syscall_times[169]++;
 	switch (id) {
 	case SYS_write:
 		ret = sys_write(args[0], (char *)args[1], args[2]);
