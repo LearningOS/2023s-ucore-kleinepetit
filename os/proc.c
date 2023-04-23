6732831b07b47e24e3fb23e093075f020703fc46
kleinepetit <xxaassdd@qq.com>
2023-04-23 09:10:51 +0800

    update

--- os/proc.c
+++ os/proc.c
@@ -88,6 +88,7 @@ void scheduler(void)
 				* LAB1: you may need to init proc start time here
 				*/
 				p->info->time = get_cycle() * 1000 / CPU_FREQ;
+				memset(theinfo.syscall_times,0,MAX_SYSCALL_NUM);
 				p->info->status = Running;
 				p->state = RUNNING;
 				current_proc = p;
